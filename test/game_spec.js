72a6728b0272c54c9c85f417fa91eabf9a13651f
Jesse Buchanan <hello@jessebuchanan.ca>
2015-12-05 19:57:47 -0500

    remove duplicate test

--- test/game_spec.js
+++ test/game_spec.js
@@ -27,25 +27,6 @@ describe('game logic', () => {
     }));
   });
 
-  it('handles adding the first and second player', () => {
-    const player1 = 'abcd1234';
-    const player2 = 'efgh5678';
-
-    let nextState = addPlayer(Map(), player1);
-    expect(nextState).to.equal(fromJS({
-      playerCount: 1,
-      players: ['abcd1234'],
-      gameStarted: false,
-    }));
-
-    nextState = addPlayer(nextState, player2);
-    expect(nextState).to.equal(fromJS({
-      playerCount: 2,
-      players: ['abcd1234', 'efgh5678'],
-      gameStarted: false,
-    }));
-  });
-
   it('ignores adding players when game is started', () => {
     const state = fromJS({
       playerCount: 2,
