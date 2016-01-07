82f95ba0091ae686bbf38a389686635bc9a4d071
Jesse Buchanan <hello@jessebuchanan.ca>
2016-01-07 16:49:57 -0500

    Implement `scoreHand`. Add JSDoc to utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,18 @@
 /* eslint new-cap: [2, {capIsNewExceptions: ["Map", "List"]}] */
 import shuffleArray from 'shuffle-array';
-import { List } from 'immutable';
+import { List, fromJS } from 'immutable';
+import _ from 'underscore';
 import * as Constants from './constants';
+import 'babel-polyfill'; // XXX HACK FIXME for Array.prototype.includes :(
 
-// Examples:
-//  getNextPlayer(['a', 'b'],      'a') => 'b'
-//  getNextPlayer(['a', 'b', 'c'], 'c') => 'a'
+/**
+ * Returns the name of the next player.
+ * @param {string[]} players The list of players.
+ * @param {string} currentPlayer The current player.
+ * @returns {string} The next player in sequence, looping back to the first player.
+ * @example getNextPlayer(['a', 'b'], 'a');      // => 'b'
+ * @example getNextPlayer(['a', 'b', 'c'], 'c'); // => 'a'
+ */
 export function getNextPlayer(players, currentPlayer) {
   const playerIndex = players.indexOf(currentPlayer);
   const playerOrdinal = playerIndex + 1;
@@ -25,8 +32,17 @@ export function getNextPlayer(players, currentPlayer) {
   return players[playerOrdinal];
 }
 
-// XXX no test written here
+/**
+ * Advances the game state to the next player in sequence.
+ * @param {Map} state The top-level Thirty-One game state tree.
+ * @returns {Map} A modified Thirty-One game state tree.
+ **/
 export function advanceCurrentPlayer(state) {
+  // XXX no test written here
+  // XXX this doesn't really belong here, anything that touches
+  // `state` should be in one place IMHO
+  // XXX really this is just a wrapper around `getNextPlayer`
+
   const players = state.get('players');
   const currentPlayer = state.get('currentPlayer');
   const nextPlayer = getNextPlayer(players.toArray(), currentPlayer);
@@ -34,11 +50,71 @@ export function advanceCurrentPlayer(state) {
   return state.set('currentPlayer', nextPlayer);
 }
 
+/**
+ * Given an array of cards, returns a new shuffled array of cards.
+ * @param {string[]} deck An array of cards (formatted e.g. 'Kc' or '3d')
+ * @returns {string[]} A new, shuffled array of cards.
+ **/
 export function shuffle(deck) {
   return shuffleArray(deck, { copy: true });
 }
 
+/**
+ * Generates a new, shuffled deck.
+ * @returns {string[]} A new, shuffled deck of cards.
+ **/
 export function getShuffledDeck() {
   const shuffled = shuffle(Constants.DEFAULT_DECK);
   return List(shuffled);
+}
+
+/**
+ * Returns the ordinal rank of the card.
+ * @param {string} cardName The name of the card, e.g. '4' or 'K'.
+ * @returns {number} The ordinal rank of the card, e.g. aces are worth 11.
+ */
+function _getRank(cardName) {
+  const faceCardRanks = {
+    'J': 10,
+    'K': 10,
+    'Q': 10,
+    'A': 11,
+  };
+  const rank = faceCardRanks[cardName];
+  if (rank === undefined) {
+    return parseInt(cardName, 10);
+  }
+
+  return rank;
+}
+
+/**
+ * Attempts to parse the given string into a card Map.
+ * @param {string} fullCardString The string representing the card, e.g. '10d' or 'Kc'
+ * @returns {Map} The parsed card, with keys 'name', 'rank' and 'suit'. Returns `undefined` if the card could not be parsed.
+ */
+export function parseCard(fullCardString) {
+  const regex = /([2-9]|10|J|K|Q|A)([cdhs])/;
+  const matches = fullCardString.match(regex);
+  if (!matches) {
+    return undefined;
+  }
+  const [name, suit] = matches.slice(1); // slice: matches[0] is original string
+  const rank = _getRank(name);
+  return fromJS({ rank, name, suit });
+}
+
+/**
+ * Returns the numeric score of the hand according to the rules of Thirty-One.
+ * @param {List<Map>} hand The Thirty-One hand to be scored.
+ * @returns {number} The computed score of the hand.
+ */
+export function scoreHand(hand) {
+  const cards = hand.map(parseCard);
+  const ranksBySuit = { c: 0, d: 0, h: 0, s: 0 };
+  cards.forEach((card) => {
+    ranksBySuit[card.get('suit')] += card.get('rank');
+  });
+
+  return _.max(Object.values(ranksBySuit));
 }
--- test/scoring_spec.js
+++ test/scoring_spec.js
@@ -5,7 +5,7 @@
 
 import { expect } from 'chai';
 import { fromJS } from 'immutable';
-import { parseCard } from '../src/utils';
+import { parseCard, scoreHand } from '../src/utils';
 
 describe('parseCard', () => {
   it('numbered cards are worth their rank', () => {
@@ -22,11 +22,32 @@ describe('parseCard', () => {
     expect(parseCard('Kd')).to.equal(fromJS({
       name: 'K', rank: 10, suit: 'd',
     }));
+
+    expect(parseCard('Jc')).to.equal(fromJS({
+      name: 'J', rank: 10, suit: 'c',
+    }));
+
+    expect(parseCard('Qh')).to.equal(fromJS({
+      name: 'Q', rank: 10, suit: 'h',
+    }));
   });
 
   it('aces are worth 11', () => {
-    expect(parseCard('Kd')).to.equal(fromJS({
-      name: 'A', rank: 11, suit: 'd',
+    expect(parseCard('As')).to.equal(fromJS({
+      name: 'A', rank: 11, suit: 's',
     }));
+  });
+});
+
+describe('scoreHand', () => {
+  it('can score a hand of all the same suit', () => {
+    const hand = ['10c', 'Jc', 'Ac'];
+    const score = scoreHand(hand);
+    expect(score).to.equal(31);
+  });
+
+  it('can score a hand of varying suits', () => {
+    expect(scoreHand(['10c', '9c', '2s'])).to.equal(19);
+    expect(scoreHand(['10c', '9h', '2s'])).to.equal(10);
   });
 });
