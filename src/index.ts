3c7a3204404f31f63711ceb367504e195cfb80a1
Jesse Buchanan <hello@jessebuchanan.ca>
2020-09-04 02:13:31 -0400

    pusher?

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import express from 'express'
+import Pusher from 'pusher'
 
 const app = express()
 
+if (!process.env.PUSHER_URL) {
+  throw new Error('PUSHER_URL not set')
+}
+
+const pusher = Pusher.forURL(process.env.PUSHER_URL)
+
 app.use('*', (req, res) => {
-  res.send('hello world')
+  const payload = {
+    'message': new Date().toString()
+  }
+  pusher.trigger('my-channel', 'my-event', {
+    'message': new Date().toString()
+  });
+  res.send(`Pushed payload ${JSON.stringify(payload)}`)
 })
 
 app.listen(process.env.PORT || 8080, () => {
