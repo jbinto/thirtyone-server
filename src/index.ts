cf6fc2f647a28d59f7cb18d53f665f727f6858b1
Jesse Buchanan <hello@jessebuchanan.ca>
2020-09-04 02:49:45 -0400

    typo

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,9 @@ app.get('/', (req, res) => {
   const payload = {
     'message': new Date().toString()
   }
-  pusher.trigger('my-channel', 'my-event', payload);
+  pusher.trigger('my-channel', 'my-event', payload)
   res.send(`Pushed payload ${JSON.stringify(payload)}`)
+})
 
 app.listen(process.env.PORT || 8080, () => {
   console.log('Listening....')
